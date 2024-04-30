6e568d308af0f3577602e01c02b89988638425df
LydiaStoneKonstanski <yourname@example.com>
2024-04-30 13:13:10 -0400

    Created MySql Tables

--- phase1/create_db.py
+++ phase1/create_db.py
@@ -1,15 +1,34 @@
-import mysql.connect
+import mysql
+from mysql import connector
 import name_password
 
-mydb = mysql.connector.connect(
-    host='localhost',
-    user= name_password.username,
-    passwd= name_password.password,
-)
+try:
+    connection = mysql.connector.connect(
+        host='localhost',
+        database= 'zipbank',
+        user= name_password.username,
+        passwd= name_password.password
+    )
+    my_cursor = connection.cursor()
 
-my_cursor = mydb.cursor()
-
-my_cursor.execute("CREATE DATABASE zipbank")
-my_cursor.execute("SHOW DATABASES")
-for db in my_cursor:
-    print(db)
+    mySql_Create_Table_Query = """ CREATE TABLE customers (
+                            id INT PRIMARY KEY,
+                            name VARCHAR(255),
+                            balance DECIMAL(10,2) NOT NULL
+                            )
+                        
+                            CREATE TABLE ledger(
+                            id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
+                            transaction_date INT, 
+                            amount DECIMAL (10,2) NOT NULL,
+                            transaction_type ENUM('deposit', 'withdrawal')
+                            )"""
+    result = my_cursor.execute(mySql_Create_Table_Query)
+    print("tables created successfully")
+except mysql.connector.Error as error:
+    print("failed to create table in MySql: {}".format(error))
+finally:
+    if connection.is_connected():
+        my_cursor.close()
+        connection.close()
+        print("mySQL connection is closed")
